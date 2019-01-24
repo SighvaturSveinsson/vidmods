a1cb2b199e8524480040c9837ca863ab44c571b4
SighvaturSveinsson <sighvatursv373@nemi.tskoli.is>
2019-01-24 12:52:48 +0000

    Add files via upload

---
+++ morph.js
@@ -0,0 +1,15 @@
+/*
+var svg = document.getElementById("logos");
+var s = Snap(svg);
+var simpleCup = Snap.select('#v1');
+var fancyCup = Snap.select('#v2');
+var simpleCupPoints = simpleCup.node.getAttribute('d');
+var fancyCupPoints = fancyCup.node.getAttribute('d');
+var toFancy = function(){
+  simpleCup.animate({ d: fancyCupPoints }, 1000, mina.backout, toSimple);  
+}
+var toSimple = function(){
+  simpleCup.animate({ d: simpleCupPoints }, 1000, mina.backout, toFancy); 
+}
+toSimple();
+*/
